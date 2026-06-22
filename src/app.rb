10f2bb40e69b1b9eedef1fa7e27599a73e5a173e
VitorVT9 <vsilvab57@gmail.com>
2026-06-22 00:17:04 -0300

    feat: cria teste inicial de comunicação entre classe buscaendereco

--- src/app.rb
+++ src/app.rb
@@ -1,4 +1,16 @@
 require_relative "cep_finder.rb"
 
 puts "====Localizador de CEP===="
+puts ""
 
+print "Digite o seu CEP (apenas números): "
+cep_digitado = gets.chomp
+
+buscador = CepFinder.new(cep_digitado)
+
+puts "\nBuscando informações..."
+
+resultado = buscador.buscaendereco
+
+puts ""
+puts resultado
--- src/cep_finder.rb
+++ src/cep_finder.rb
@@ -1,9 +1,14 @@
 class CepFinder
   def initialize(cep)
-
+    @cep = cep
   end
 
   def buscaendereco
+    @logradouro = "Rua dos Bobos"
+    @bairro = "Centro"
+    @localidade = "Ijuí"
+    @uf = "RS"
+    "Endereço: #{@logradouro}, #{@bairro} - #{@localidade}/#{@uf}"
 
   end
 
