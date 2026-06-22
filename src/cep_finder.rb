9ac88ea6ff9d27367634f40a22574d90ee070d54
VitorVT9 <vsilvab57@gmail.com>
2026-06-22 00:39:23 -0300

    feat: estrutura método para buscar informações com base no CEP

--- src/cep_finder.rb
+++ src/cep_finder.rb
@@ -8,7 +8,17 @@ class CepFinder
   end
 
   def buscaendereco
-  
+    url_string = "https://viacep.com.br/ws/#{@cep}/json/"
+    uri = URI(url_string)
+    response = Net::HTTP.get(uri)
+    dados_da_api = JSON.parse(response)
+
+    @logradouro = dados_da_api["logradouro"]
+    @bairro = dados_da_api["bairro"]
+    @localidade = dados_da_api["localidade"]
+    @uf = dados_da_api["uf"]
+    
+    "Endereço: #{@logradouro}, #{@bairro} - #{@localidade}/#{@uf}"
   end
 
   def salvaendereco
